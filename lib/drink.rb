9269eb78db2e30373d1765c6b6aa2036e379619d
ddgg7755 <ddgg7755@gmail.com>
2019-02-20 14:03:11 +0900

    Drinkクラスの不要なコメントやスペースの削除

--- lib/drink.rb
+++ lib/drink.rb
@@ -1,7 +1,6 @@
 class Drink
   attr_reader :name, :price
-  
-  # 初期状態
+ 
   def initialize(name,price)
     @name = name
     @price = price
@@ -11,22 +10,15 @@ class Drink
     {name: @name, price: @price}
   end
 
-  # コーラ情報
   def self.coke
     Drink.new("Coke",120)
   end
+
   def self.red_bull
     Drink.new("Red_bull",200)
   end
+
   def self.water
     Drink.new("Water",100)
   end
 end
-
-# drink1 = Drink.coke
-# drink2 = Drink.red_bull
-# drink3 = Drink.water
-# # require 'byebug'; byebug
-# puts drink1
-# puts drink2
-# puts drink3
