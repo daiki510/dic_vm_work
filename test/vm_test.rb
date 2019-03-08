603da479b1f35257da0e5a0ed2b7ad2ed50efc05
ddgg7755 <ddgg7755@gmail.com>
2019-03-08 15:24:00 +0900

    テストコードの修正

--- test/vm_test.rb
+++ test/vm_test.rb
@@ -1,15 +1,32 @@
 require 'minitest/autorun'
-# require './lib/drink'
+require './lib/drink'
 require './lib/vm'
 
 class VendingMachineTest < Minitest::Test
   
-  def set_up
-    vm = VendingMachine.new
+  def setup
+    @vm = VendingMachine.new
+    @vm.insert(500)
   end
   
-  def check_current_money
-    vm.insert(500)
-    assert_equal vm.current_total_money, 500
+  def test_current_money
+    assert_equal 500,@vm.current_total_money
   end
+
+  def test_return_money
+    @vm.return_money
+    assert_equal 0,@vm.current_total_money
+  end
+  
+  def test_select_drink
+    @vm.judge
+    @vm.select_drink(1)
+    assert_equal 380,@vm.current_total_money
+    assert_equal 120,@vm.current_total_sales
+  end
+
+  # def test_stock_drink
+  #   @vm.store_drink(:Coke, 5)
+  #   assert_equal 5,@stocks[:Coke]
+  # end
 end
